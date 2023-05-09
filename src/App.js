3c363db8ba495bd469fe57b009491f68ba166ae9
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-05-09 09:25:51 -0700

    add functionality to modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-function App() {
+const App = () => {
+  const [cartShow, setCartShow] = useState(false);
+  const showCartHandler = () => {
+    setCartShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartShow(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartShow && <Cart show={cartShow} onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
     </Fragment>
   );
-}
+};
 
 export default App;
